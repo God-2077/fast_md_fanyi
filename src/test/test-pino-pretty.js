7ab435eb15dc8a08ebcb79988816b13be58b66f0
kissablecho <god_2077@outlook.com>
2026-04-28 18:27:03 +0800

    test(pino-pretty): 添加数组和对象日志输出测试用例
    添加测试用例验证pino-pretty对数组和复杂对象的格式化输出功能

--- src/test/test-pino-pretty.js
+++ src/test/test-pino-pretty.js
@@ -22,3 +22,42 @@ logger.error({ err: new Error('test error') }, 'Error with object')
 logger.debug('Debug message')
 
 logger.child({ module: 'auth' }).info('Child logger')
+
+const list = [1, 2, 3, 4, 5]
+logger.info( list )
+// [2026-04-28 18:25:25] INFO:
+//     0: 1
+//     1: 2
+//     2: 3
+//     3: 4
+//     4: 5
+
+const obj = {
+  name: 'kissablecho',
+  age: 22,
+  sex: 'male'
+}
+logger.info( obj )
+// [2026-04-28 18:25:25] INFO (kissablecho):
+//     age: 22
+//     sex: "male"
+
+// 复杂的 object
+// name 字段会作为 [2026-04-28 18:25:25] INFO (kissablecho) 中的 (kissablecho)
+const complexObj = {
+  name: 'kissablecho',
+  age: 22,
+  sex: 'male',
+  address: {
+    city: 'Beijing',
+    street: '100000'
+  }
+}
+logger.info( complexObj )
+// [2026-04-28 18:25:25] INFO (kissablecho):
+//     age: 22
+//     sex: "male"
+//     address: {
+//         city: "Beijing"
+//         street: "100000"
+//     }
